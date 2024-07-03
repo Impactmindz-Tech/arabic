6a8ceb3aa5e8b5d4cdecaa8ed9145fa5c9be780c
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-03 17:48:09 +0530

    updated

--- src/components/slider/SwiperSlider.jsx
+++ src/components/slider/SwiperSlider.jsx
@@ -1,28 +1,12 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-const breakPoints = {
-  // when window width is >= 320px
-  200: {
-    slidesPerView: 3,
-    spaceBetween: 2,
-  },
-  // when window width is >= 480px
-  480: {
-    slidesPerView: 3,
-    spaceBetween: 10,
-  },
-  // when window width is >= 640px
-  640: {
-    slidesPerView: 6,
-    spaceBetween: 1,
-  },
-};
+
 const SwiperSlider = ({ imagesslider, category }) => {
   return (
     <Swiper
       spaceBetween={1}
       slidesPerView={category ? 6 : 1}
-      breakpoints={breakPoints}
+    
     >
       {imagesslider?.map((item, index) => {
         return (
